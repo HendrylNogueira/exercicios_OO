3b1309804f30c49ab17c77d21f9a219777f308dd
Hendryl Nogueira <hendrylnogueira2508@gmail.com>
2024-02-15 08:11:26 -0300

    adicionando ex02

---
+++ exercicio02/exercicio02/Funcionario.cs
@@ -0,0 +1,10 @@
+﻿
+namespace exercicio02
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public double Media;
+    }
+}
---
+++ exercicio02/exercicio02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F1 = new Funcionario();
+            Funcionario F2 = new Funcionario();
+
+            Console.WriteLine($"Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            F1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            F1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            F2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            F2.Salario = double.Parse(Console.ReadLine());
+
+            double media = (F1.Salario + F2.Salario) / 2;
+
+            Console.WriteLine($"Salário médio: {media:F2}");
+        }
+    }
+}
