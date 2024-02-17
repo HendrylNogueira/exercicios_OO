dcb6c6455c4851549d5fad2095bcd5149598b779
Hendryl Nogueira <hendrylnogueira2508@gmail.com>
2024-02-16 23:28:30 -0300

    aprendendo sobre metodos

---
+++ exercicio03/exercicio03/Produto.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace exercicio03
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome $ {Preco:F2}, {Quantidade} unidades, Total: $ {ValorTotalEmEstoque():F2}";
+        }
+    }
+}
