7095f386c77835959376cef7dd3f0bf30b37e0fd
Hendryl Nogueira <hendrylnogueira2508@gmail.com>
2024-02-19 07:22:43 -0300

    Fixando conteudo de classes

---
+++ exercicio04/exercicio04/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            
+            Console.WriteLine("Digite a largura e altura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+            retangulo.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Area: {retangulo.Area():F2}");
+            Console.WriteLine($"Perimetro: {retangulo.Perimetro():F2}");
+            Console.WriteLine($"Diagonal: {retangulo.Diagonal():F2}");
+
+            Console.Read();
+        }
+    }
+}
