233edcb0f1078fa3e3aed538319081966d35f285
Hendryl Nogueira <hendrylnogueira2508@gmail.com>
2024-02-23 21:11:26 -0300

    ex06 de orientacao a objetos

---
+++ exercicio06/exercicio06/Aluno.cs
@@ -0,0 +1,29 @@
+﻿
+namespace exercicio06
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return (Nota1 + Nota2 + Nota3);
+        }
+
+        public void Aprovado()
+        {
+            Console.WriteLine("APROVADO");
+        }
+
+        public void Reprovado()
+        {
+            double nota_faltante = 60 - NotaFinal();
+            Console.WriteLine($"REPROVADO \n" +
+                $"FALTARAM {nota_faltante:F2} PONTOS");
+        }
+
+    }
+}
---
+++ exercicio06/exercicio06/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno_x = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno_x.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            
+            aluno_x.Nota1 = double.Parse(Console.ReadLine());
+            aluno_x.Nota2 = double.Parse(Console.ReadLine());
+            aluno_x.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"NOTA FINAL = {aluno_x.NotaFinal():F2}");
+
+            if (aluno_x.NotaFinal() >= 60)
+            {
+                aluno_x.Aprovado();
+            }
+            else
+            {
+                aluno_x.Reprovado();
+            }
+        }
+    }
+}
